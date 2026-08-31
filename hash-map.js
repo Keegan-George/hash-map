d74476ced323ddaa76033c73145b228bc75c9b75
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-08-31 15:29:48 -0400

    Implement set for adding key:value pair to hash map. Currently, the key:value pair is stored as a string

--- hash-map.js
+++ hash-map.js
@@ -1,10 +1,13 @@
+import { LinkedList } from "../linked-list/linked-list.js";
+
 class HashMap {
   constructor() {
-    this._loadFactor = null;
-    this._capacity = null;
+    this._loadFactor = 0.75;
+    this._capacity = 16;
+    this._buckets = [];
   }
 
-  hash(key) {
+  #hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
@@ -13,5 +16,18 @@ class HashMap {
     }
 
     return hashCode;
+  }
+
+  set(key, value) {
+    const hashCode = this.#hash(key);
+    const bucket = hashCode % this._capacity;
+
+    if (!this._buckets[bucket]) {
+      const list = new LinkedList();
+      list.append(`${key}:${value}`);
+      this._buckets[bucket] = list;
+    } else {
+      this._buckets[bucket].append(`${key}:${value}`);
+    }
   }
 }
