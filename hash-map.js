5dc2404e17d6368a183ba1fe4f6e0560845d11d4
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-08-31 12:05:50 -0400

    Add hash function

--- hash-map.js
+++ hash-map.js
@@ -3,4 +3,15 @@ class HashMap {
     this._loadFactor = null;
     this._capacity = null;
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
 }
