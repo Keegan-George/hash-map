520478ef2036b336436fa402043d793b01a26821
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-09-01 16:31:58 -0400

    Update tests to align with data set shared in assignment

--- hash-map.js
+++ hash-map.js
@@ -80,21 +80,4 @@ class HashMap {
   }
 }
 
-// const map = new HashMap();
-// map.set("apple", "red");
-// map.set("banana", "yellow");
-// map.set("carrot", "orange");
-// map.set("dog", "brown");
-// map.set("elephant", "gray");
-// map.set("frog", "green");
-// map.set("grape", "purple");
-// map.set("hat", "black");
-// map.set("ice cream", "white");
-// map.set("jacket", "blue");
-// map.set("kite", "pink");
-// map.set("lion", "golden");
-
-// map.remove("hat");
-// const stop = "stop";
-
 export { HashMap };
--- hash-map.test.js
+++ hash-map.test.js
@@ -6,41 +6,51 @@ let map;
 describe("Positive cases", () => {
   beforeEach(() => {
     map = new HashMap();
-    map.set("Peter", "Parker");
-    map.set("Bruce", "Wayne");
-    map.set("Clark", "Kent");
-    map.set("Eric", "Brooks");
+    map.set("apple", "red");
+    map.set("banana", "yellow");
+    map.set("carrot", "orange");
+    map.set("dog", "brown");
+    map.set("elephant", "gray");
+    map.set("frog", "green");
+    map.set("grape", "purple");
+    map.set("hat", "black");
+    map.set("ice cream", "white");
+    map.set("jacket", "blue");
+    map.set("kite", "pink");
+    map.set("lion", "golden");
   });
 
-  describe("get() scenarios", () => {
-    test("Get node value", () => {
-      expect(map.get("Peter")).toBe("Parker");
-      expect(map.get("Bruce")).toBe("Wayne");
-      expect(map.get("Clark")).toBe("Kent");
-      expect(map.get("Eric")).toBe("Brooks");
+  describe("set() scenarios", () => {
+    test("add node", () => {
+      expect(map.get("ebony")).toBeNull();
+      expect(map.has("ebony")).toBe(false);
+      map.set("ebony", "blade");
+      expect(map.get("ebony")).toBe("blade");
+      expect(map.has("ebony")).toBe(true);
     });
 
-    test("Update node value", () => {
-      expect(map.get("Bruce")).toBe("Wayne");
-      map.set("Bruce", "Banner");
-      expect(map.get("Bruce")).toBe("Banner");
-    });
-
-    test("Non-existant node returns null", () => {
-      expect(map.get("Tony")).toBeNull();
+    test("update node", () => {
+      expect(map.get("dog")).toBe("brown");
+      expect(map.has("dog")).toBe(true);
+      map.set("dog", "cane corso");
+      expect(map.get("dog")).toBe("cane corso");
+      expect(map.has("dog")).toBe(true);
     });
   });
 
-  describe("has() scenarios", () => {
-    test("Existant nodes return true", () => {
-      expect(map.has("Peter")).toBe(true);
-      expect(map.has("Bruce")).toBe(true);
-      expect(map.has("Clark")).toBe(true);
-      expect(map.has("Eric")).toBe(true);
+  describe("remove() scenarios", () => {
+    test("Remove node", () => {
+      expect(map.get("jacket")).toBe("blue");
+      expect(map.has("jacket")).toBe(true);
+      expect(map.remove("jacket")).toBe(true);
+      expect(map.get("jacket")).toBeNull();
+      expect(map.has("jacket")).toBe(false);
     });
 
-    test("Non-existant node return false", () => {
-      expect(map.has("Tony")).toBe(false);
+    test("Remove non-existant node returns false", () => {
+      expect(map.get("purple")).toBeNull();
+      expect(map.has("purple")).toBe(false);
+      expect(map.remove("purple")).toBe(false);
     });
   });
 });
