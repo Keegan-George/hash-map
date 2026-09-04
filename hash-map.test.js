f12cb5351cc2eb62e6764e6e9d700b2ff89064b8
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-09-04 09:33:23 -0400

    Extract node data to a variable  that is accessible by all tests

--- hash-map.test.js
+++ hash-map.test.js
@@ -3,21 +3,25 @@ import { test, expect, describe, beforeEach } from "@jest/globals";
 
 let map;
 
+const NODE_DATA = {
+  apple: "red",
+  banana: "yellow",
+  carrot: "orange",
+  dog: "brown",
+  elephant: "gray",
+  frog: "green",
+  grape: "purple",
+  hat: "black",
+  "ice cream": "white",
+  jacket: "blue",
+  kite: "pink",
+  lion: "golden",
+};
+
 describe("Positive cases", () => {
   beforeEach(() => {
     map = new HashMap();
-    map.set("apple", "red");
-    map.set("banana", "yellow");
-    map.set("carrot", "orange");
-    map.set("dog", "brown");
-    map.set("elephant", "gray");
-    map.set("frog", "green");
-    map.set("grape", "purple");
-    map.set("hat", "black");
-    map.set("ice cream", "white");
-    map.set("jacket", "blue");
-    map.set("kite", "pink");
-    map.set("lion", "golden");
+    Object.entries(NODE_DATA).forEach(([k, v]) => map.set(k, v));
   });
 
   describe("Add/Update scenarios", () => {
